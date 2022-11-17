5d76de0e99380554b46db1d3237d4fa4b6931b1a
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2022-11-17 12:22:21 +1100

    Trigger Volume start

---
+++ Assets/Scripts/TriggerVolume.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerVolume : MonoBehaviour
+{
+    public UnityEvent OnEnterTrigger;
+
+    private Collider _collider;
+    private void Awake()
+    {
+        _collider = GetComponent<Collider>();
+        _collider.isTrigger = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // validate object
+        Debug.Log("Entered");
+        OnEnterTrigger.Invoke();
+    }
+}
