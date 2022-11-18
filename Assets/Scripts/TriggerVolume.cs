396bfca421c6c963e9f78594ca3dc821171f3d6f
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2022-11-18 15:24:00 +1100

    committed

--- Assets/Scripts/TriggerVolume.cs
+++ Assets/Scripts/TriggerVolume.cs
@@ -3,20 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class TriggerVolume : MonoBehaviour
+namespace ConnorLuck
 {
-    public UnityEvent OnEnterTrigger;
+    // prevents this from running without a collider component being attached - just another safety net
+    // it could be further specified to be BoxCollider - but this is not essential
+    [RequireComponent(typeof(Collider))]  
 
-    private Collider _collider;
-    private void Awake()
+    public class TriggerVolume : MonoBehaviour
     {
-        _collider = GetComponent<Collider>();
-        _collider.isTrigger = true;
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        // validate object
-        Debug.Log("Entered");
-        OnEnterTrigger.Invoke();
+
+        [Header("Settings")]
+        [SerializeField] private bool oneShot = true; // used to turn on or off the option to trigegr this only once
+
+        [Header("Gizmo Settings")]
+        [SerializeField] private bool displayGizmos = false; // used to turn on or off the outline for the Gizmo 
+        
+        public UnityEvent OnEnterTrigger; 
+        private Collider _collider;
+        public bool alreadyEntered = false;
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
+            _collider.isTrigger = true;
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+
+            // I'm just making sure that the trigger is activtaing - It Is!
+
+
+            if (oneShot && alreadyEntered) return; // functionality to stop the trigger from firing again if it has been triggered once
+
+            Debug.Log("GET"); // here to test that the Trigger is firing
+
+            OnEnterTrigger.Invoke();
+            alreadyEntered = true;
+            
+        }
+        // functionanility to help edit the Trigger in future - so that we can remove the Mesh Renderer for gameplay
+        // relates to displayGizmos as above
+        private void OnDrawGizmos()
+        {
+
+            if (displayGizmos == false)   return;
+
+            if (_collider == null)
+            {
+                _collider = GetComponent<Collider>();
+            }
+            Gizmos.DrawCube(transform.position, _collider.bounds.size);
+        }
+        private void OnDrawGizmosSelected()
+        {
+            if (displayGizmos == false)
+                return;
+        }
     }
 }
