0fd552ed9d893d8679f40fe62ce21aeb496fb739
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2022-11-21 20:49:25 +1100

    more work on setting up the button

--- Assets/Scripts/SlideDoor.cs
+++ Assets/Scripts/SlideDoor.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class SlideDoor : MonoBehaviour
 {
 
+    public UnityEvent OnEnterTrigger;
+    private Clickable doorSlider;
+    public bool alreadyEntered = false;
+    private void Awake()
 
+    {
+        doorSlider = GetComponent<Clickable>();
+    }
+
+    private void OnPointerClick()
+    {
+        if (doorSlider != null) ;
+    }
 
 }
